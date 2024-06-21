1a454038386ea979273516089bfa5ab206509ac9
IhateCreatingUserNames2 <dwint@live.com>
2024-06-21 05:37:31 -0300

    Update and rename MFPS_IntegrationHelper to MFPS_IntegrationHelper.cs

--- MFPS_IntegrationHelper.cs
+++ MFPS_IntegrationHelper.cs
@@ -1,32 +1,39 @@
 using UnityEngine;
-using System.Collections;
-using MFPS;
 using MFPS.InputManager;
 using FC_ParkourSystem;
+using Photon.Pun;
+using System.Collections;
 
 namespace FC_ParkourSystem
 {
     public class MFPS_IntegrationHelper : MonoBehaviour, IParkourCharacter
     {
-        ParkourController parkourController;
-        ClimbController climbController;
-        bl_FirstPersonController firstPersonController;
-        Animator animator;
-        Collider playerCollider;
-        bl_PlayerCameraSwitcher cameraSwitcher;
-        bl_PlayerAnimations playerAnimations;
-        AnimatorHelper animatorHelper;
+        private ParkourController parkourController;
+        private ClimbController climbController;
+        private bl_FirstPersonController firstPersonController;
+        private bl_PlayerAnimationsBase playerAnimations;
+        private bl_PlayerRagdollBase playerRagdoll;
+        private Collider playerCollider;
+        private bl_PlayerCameraSwitcher cameraSwitcher;
+        private bl_PlayerNetwork playerNetwork;
+        private bl_PlayerReferences playerReferences;
+        private Camera playerCamera;
 
         public bool UseRootMotion { get; set; } = false;
-        public Vector3 MoveDir => firstPersonController.transform.forward * firstPersonController.Velocity.magnitude;
+
+        public Vector3 MoveDir => new Vector3(bl_GameInput.Horizontal, 0, bl_GameInput.Vertical).normalized;
+
         public bool IsGrounded => firstPersonController.isGrounded;
+
         public float Gravity => -9.81f;
 
         public Animator Animator
         {
-            get { return animator == null ? GetComponent<Animator>() : animator; }
-            set { animator = value; }
+            get => playerReferences.PlayerAnimator;
+            set => playerReferences.PlayerAnimator = value;
         }
+
+        public bool PreventParkourAction => firstPersonController.State == PlayerState.Crouching || firstPersonController.State == PlayerState.Running;
 
         private void Awake()
         {
@@ -35,14 +42,25 @@ namespace FC_ParkourSystem
 
         private void InitializeComponents()
         {
+            Debug.Log("Initializing components in MFPS_IntegrationHelper");
+
+            playerReferences = GetComponent<bl_PlayerReferences>();
+            if (playerReferences == null)
+            {
+                Debug.LogError("PlayerReferences is missing.");
+                this.enabled = false;
+                return;
+            }
+
             parkourController = GetComponent<ParkourController>();
             climbController = GetComponent<ClimbController>();
-            firstPersonController = GetComponent<bl_FirstPersonController>();
-            playerCollider = GetComponent<Collider>();
-            animator = GetComponent<Animator>();
+            firstPersonController = playerReferences.firstPersonController as bl_FirstPersonController;
+            playerAnimations = playerReferences.playerAnimations;
+            playerRagdoll = playerReferences.playerRagdoll;
+            playerCollider = playerReferences.characterController;
             cameraSwitcher = GetComponent<bl_PlayerCameraSwitcher>();
-            playerAnimations = GetComponent<bl_PlayerAnimations>();
-            animatorHelper = GetComponent<AnimatorHelper>();
+            playerNetwork = playerReferences.playerNetwork;
+            playerCamera = playerReferences.playerCamera;
 
             if (AreRequiredComponentsMissing())
             {
@@ -57,33 +75,13 @@ namespace FC_ParkourSystem
         private bool AreRequiredComponentsMissing()
         {
             return parkourController == null || climbController == null || firstPersonController == null ||
-                   playerCollider == null || animator == null || cameraSwitcher == null || playerAnimations == null || animatorHelper == null;
+                   playerAnimations == null || playerRagdoll == null || playerCollider == null ||
+                   cameraSwitcher == null || playerNetwork == null || playerCamera == null;
         }
 
         private void InitializeAnimatorHelper()
         {
-            if (animatorHelper != null)
-            {
-                animatorHelper.initialize(animator);
-            }
-        }
-
-        public void OnEndParkourAction()
-        {
-            if (firstPersonController != null) firstPersonController.enabled = true;
-            if (playerCollider != null) playerCollider.enabled = true;
-            if (cameraSwitcher != null) cameraSwitcher.enabled = true;
-        }
-
-        public void OnStartParkourAction()
-        {
-            if (firstPersonController != null)
-            {
-                firstPersonController.Velocity = Vector3.zero;
-                firstPersonController.enabled = false;
-            }
-            if (playerCollider != null) playerCollider.enabled = false;
-            if (cameraSwitcher != null) cameraSwitcher.enabled = false;
+            // Implementation for initializing animator helper if needed
         }
 
         private void Update()
@@ -93,6 +91,7 @@ namespace FC_ParkourSystem
             {
                 firstPersonController.OnUpdate();
             }
+            UpdateCamera();
         }
 
         private void FixedUpdate()
@@ -112,13 +111,13 @@ namespace FC_ParkourSystem
         {
             float horizontal = bl_Input.HorizontalAxis;
             float vertical = bl_Input.VerticalAxis;
-            bool jump = bl_InputData.Instance.GetButtonDown("Jump");
-            bool crouch = bl_InputData.Instance.GetButtonDown("Crouch");
-            bool drop = bl_InputData.Instance.GetButtonDown("Drop");
-            bool sprint = bl_InputData.Instance.GetButton("Sprint");
+            bool jump = bl_GameInput.Jump();
+            bool crouch = bl_GameInput.Crouch();
+            bool drop = bl_GameInput.Interact(); // Assuming Interact is mapped to Drop
+            bool sprint = bl_GameInput.Run();
 
             Vector3 move = new Vector3(horizontal, 0, vertical);
-            animator.SetFloat("Speed", move.magnitude);
+            Animator.SetFloat("Speed", move.magnitude);
 
             if (move != Vector3.zero)
             {
@@ -161,7 +160,7 @@ namespace FC_ParkourSystem
         {
             if (firstPersonController != null && firstPersonController.isGrounded && parkourController != null)
             {
-                animator.SetTrigger("Jump");
+                Animator.SetTrigger("Jump");
                 parkourController.VerticalJump();
             }
         }
@@ -185,7 +184,7 @@ namespace FC_ParkourSystem
         {
             if (climbController != null && !parkourController.InAction)
             {
-                if (bl_InputData.Instance.GetButtonDown("Drop"))
+                if (bl_Input.isButtonDown("Drop"))
                 {
                     if (climbController.envScanner.DropLedgeCheck(transform.forward, out ClimbLedgeData ledgeData))
                     {
@@ -200,16 +199,47 @@ namespace FC_ParkourSystem
 
                         if (climbController.CheckWall(currentPoint).Value.isWall)
                         {
-                            animator.SetFloat("freeHang", 0);
+                            Animator.SetFloat("freeHang", 0);
                             StartCoroutine(climbController.JumpToLedge(currentPoint.transform, "DropToHang", 0.50f, 0.90f, rotateToLedge: true, matchStart: AvatarTarget.LeftFoot));
                         }
                         else
                         {
-                            animator.SetFloat("freeHang", 1);
+                            Animator.SetFloat("freeHang", 1);
                             StartCoroutine(climbController.JumpToLedge(currentPoint.transform, "DropToFreeHang", 0.50f, 0.89f, rotateToLedge: true, matchStart: AvatarTarget.LeftFoot));
                         }
                     }
                 }
+            }
+        }
+
+        private void UpdateCamera()
+        {
+            // Implement camera update logic if needed
+        }
+
+        public void OnEndParkourAction()
+        {
+            if (firstPersonController != null) firstPersonController.enabled = true;
+            if (playerCollider != null) playerCollider.enabled = true;
+            if (cameraSwitcher != null) cameraSwitcher.enabled = true;
+            if (playerRagdoll != null)
+            {
+                playerRagdoll.SetActiveRagdollPhysics(false);
+            }
+        }
+
+        public void OnStartParkourAction()
+        {
+            if (firstPersonController != null)
+            {
+                firstPersonController.Velocity = Vector3.zero;
+                firstPersonController.enabled = false;
+            }
+            if (playerCollider != null) playerCollider.enabled = false;
+            if (cameraSwitcher != null) cameraSwitcher.enabled = false;
+            if (playerRagdoll != null)
+            {
+                playerRagdoll.SetActiveRagdollPhysics(true);
             }
         }
 
@@ -221,8 +251,5 @@ namespace FC_ParkourSystem
             }
             yield break;
         }
-
-        public bool PreventParkourAction => firstPersonController != null &&
-                                            (firstPersonController.State == PlayerState.Crouching || firstPersonController.State == PlayerState.Running);
     }
 }
